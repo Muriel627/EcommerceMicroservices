b9c2ec439169bee23ae90734fc8a00f93e579f35
Muriel627 <160170203+Muriel627@users.noreply.github.com>
2026-02-11 03:57:21 -0500

    Initial commit

---
+++ src/test/java/com/luciana/EcommerceMicroservices/EcommerceMicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciana.EcommerceMicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceMicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
